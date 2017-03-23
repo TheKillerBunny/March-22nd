1667647c9bc69e70d443e929cf19cf26ba1eebe8
TheKillerBunny <ericsowell@Erics-Air.fios-router.home>
2017-03-22 19:55:21 -0500

    HIDE IT!

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ var img = document.querySelector('img');
 console.log(p);
 
 p.addEventListener('click', function(){
-  //p.classList.toggle('chosen');
   img.classList.toggle('hidden');
+
+  var containsMaybe = img.classList.contains('hidden')
+
+  if (containsMaybe === true) {
+    p.textContent = 'Show me the image!'
+  }
+  else {
+    p.textContent = 'HIDE IT!';
+  }
+
 });
